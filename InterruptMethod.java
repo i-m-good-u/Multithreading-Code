1ad66439a15792a50630d258687fb8e75f76f7d9
Guddu Kumar <guddukumar032002@gmail.com>
2024-01-20 16:04:27 +0530

    change Inerrupt from interrupted

--- InterruptMethod.java
+++ InterruptMethod.java
@@ -1,9 +1,14 @@
-public class InterruptedMethod extends Thread {
+public class InterruptMethod extends Thread {
 
     public void run() {
         try {
             for (int i = 1; i <= 5; i++) {
                 System.out.println("Thread 0 " + i);
+
+                // Interrupt method only works when thread is in either sleeping state or
+                // waiting state otherwise interrupt method behve noramal means does not affect
+                // flow of programm
+
                 Thread.sleep(1000);
             }
         } catch (Exception e) {
@@ -12,7 +17,7 @@ public class InterruptedMethod extends Thread {
     }
 
     public static void main(String[] args) {
-        InterruptedMethod interruptedMethod = new InterruptedMethod();
+        InterruptMethod interruptedMethod = new InterruptMethod();
         interruptedMethod.start();
         interruptedMethod.interrupt();
 
